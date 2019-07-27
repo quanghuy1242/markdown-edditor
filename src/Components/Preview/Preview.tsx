864bfde91863d5fc9dd664e7bbe741d2c17e406a
quanghuy1242 <quanghuy1242@gmail.com>
2019-07-27 17:21:57 +0700

    render value editor as html

--- src/Components/Preview/Preview.tsx
+++ src/Components/Preview/Preview.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 import { useStyles } from './Preview.style';
 import { useSelector } from 'react-redux';
 import { IState } from '../../store/types';
+import MarkdownIt from 'markdown-it';
+
+const md = MarkdownIt();
 
 const Preview: React.FC = () => {
   const classes = useStyles();
   const isToggle = useSelector((state: IState) => state.isTogglePreview);
   const value = useSelector((state: IState) => state.value);
   return isToggle ? (
-    <div className={classes.previewWrapper}>
-      {value}
+    <div className={classes.previewWrapper} dangerouslySetInnerHTML={{ __html: md.render(value) }}>
     </div>
   ) : null;
 }
