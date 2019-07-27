9242a884ff1e94bc3bf3630e541553a73d407303
quanghuy1242 <quanghuy1242@gmail.com>
2019-07-27 22:19:54 +0700

    removed padding top of preview

--- src/Components/Preview/Preview.style.tsx
+++ src/Components/Preview/Preview.style.tsx
@@ -3,10 +3,10 @@ import { mergeStyleSets } from 'office-ui-fabric-react';
 export const useStyles = () => {
   return mergeStyleSets({
     previewWrapper: {
-      padding: '1rem',
+      padding: '0rem 1rem 1rem 1rem',
       width: '50%',
       overflowY: 'auto',
-      height: 'calc(100vh - 44px - 4rem)'
+      height: 'calc(100vh - 44px - 3rem)'
     }
   });
 }
